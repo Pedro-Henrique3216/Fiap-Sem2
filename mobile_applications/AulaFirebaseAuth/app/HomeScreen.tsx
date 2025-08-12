da806cd5018d95df9fefacb6e2c875fa2fb17a4e
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-12 08:41:05 -0300

    feat: add logout function

--- mobile_applications/AulaFirebaseAuth/app/HomeScreen.tsx
+++ mobile_applications/AulaFirebaseAuth/app/HomeScreen.tsx
@@ -1,10 +1,22 @@
-import { Text } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useRouter } from "expo-router";
+import { Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
+    const router = useRouter();
+    const logoff = async () => {
+        await AsyncStorage.removeItem("@user");
+        router.push("/");
+    }
     return (
         <SafeAreaView>
             <Text>Bem-vindo à tela inicial!</Text>
+            <TouchableOpacity onPress={logoff}>
+                <Text>
+                    Logout
+                </Text>
+            </TouchableOpacity>
         </SafeAreaView>
     );
 }
